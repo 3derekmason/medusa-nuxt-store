b6719b95956880b3b8a953c20cbc6a94f7188cb9
Derek Mason <3derekmason@gmail.com>
2023-05-24 09:29:33 -0600

    build: all products page init

---
+++ nuxt-storefront/pages/products/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="productsPage">
+    <div>
+      <h1>All Products</h1>
+    </div>
+    <div v-if="products.length" class="productContainer">
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :item="product"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const client = useMedusaClient();
+const { products } = await client.products.list();
+</script>
+
+<style>
+.productsPage {
+  padding: 0.5em;
+  width: 100vw;
+}
+
+.productContainer {
+  margin-top: 1em;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1em;
+  padding: 0.5em;
+  border: 1px solid var(--primary-main);
+}
+</style>
