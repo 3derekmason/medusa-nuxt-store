2b1108353753be29272250731d8041e9b4830486
Derek Mason <3derekmason@gmail.com>
2023-05-26 13:23:54 -0600

    build: checkout steps init

---
+++ nuxt-storefront/components/checkout/ShippingAddress.vue
@@ -0,0 +1,36 @@
+<template>
+  <form action="" class="shippingAddress">
+    <p>Shipping Address:</p>
+    <div class="row">
+      <label for="company">Company Name:</label
+      ><input type="text" name="company" />
+    </div>
+    <div class="row">
+      <label for="firstName">First Name:</label
+      ><input type="text" name="firstName" />
+    </div>
+    <div class="row">
+      <label for="lastName">Last Name:</label
+      ><input type="text" name="lastName" />
+    </div>
+    <div class="row">
+      <label for="address1">Address 1:</label
+      ><input type="text" name="address1" />
+    </div>
+    <div class="row">
+      <label for="address2">Address 2:</label
+      ><input type="text" name="address2" />
+    </div>
+    <div class="row">
+      <label for="city">City:</label><input type="text" name="city" />
+    </div>
+    <div class="row">
+      <label for="province">Province:</label
+      ><input type="text" name="province" />
+    </div>
+    <div class="row">
+      <label for="postalCode">Postal Code:</label
+      ><input type="text" name="postalCode" />
+    </div>
+  </form>
+</template>
--- nuxt-storefront/pages/checkout.vue
+++ nuxt-storefront/pages/checkout.vue
@@ -1,39 +1,23 @@
 <template>
   <div class="checkoutPage">
-    <form action="" class="shippingAddress">
-      <p>Shipping Address:</p>
-      <div class="row">
-        <label for="company">Company Name:</label
-        ><input type="text" name="company" />
+    <span class="steps">
+      <h2 :class="currentStep === 1 ? 'current' : ''">1</h2>
+      <p>></p>
+      <h2 :class="currentStep === 2 ? 'current' : ''">2</h2>
+      <p>></p>
+      <h2 :class="currentStep === 3 ? 'current' : ''">3</h2>
+    </span>
+    <div class="currentStep">
+      <div v-if="currentStep === 1">
+        <CheckoutShippingAddress />
       </div>
-      <div class="row">
-        <label for="firstName">First Name:</label
-        ><input type="text" name="firstName" />
-      </div>
-      <div class="row">
-        <label for="lastName">Last Name:</label
-        ><input type="text" name="lastName" />
-      </div>
-      <div class="row">
-        <label for="address1">Address 1:</label
-        ><input type="text" name="address1" />
-      </div>
-      <div class="row">
-        <label for="address2">Address 2:</label
-        ><input type="text" name="address2" />
-      </div>
-      <div class="row">
-        <label for="city">City:</label><input type="text" name="city" />
-      </div>
-      <div class="row">
-        <label for="province">Province:</label
-        ><input type="text" name="province" />
-      </div>
-      <div class="row">
-        <label for="postalCode">Postal Code:</label
-        ><input type="text" name="postalCode" />
-      </div>
-    </form>
+      <div v-if="currentStep === 2">Shipping Options</div>
+      <div v-if="currentStep === 3">Review and place order</div>
+    </div>
+    <span class="stepNav">
+      <button :disabled="currentStep === 1" @click="currentStep--">Prev</button>
+      <button :disabled="currentStep === 3" @click="currentStep++">Next</button>
+    </span>
   </div>
 </template>
 
@@ -41,4 +25,76 @@
 import { useMainStore } from "~/store/main";
 const client = useMedusaClient();
 const main = useMainStore();
+const currentStep = ref(1);
 </script>
+
+<style>
+.checkoutPage {
+  padding: 2em;
+}
+
+.steps {
+  color: #212121;
+}
+
+.steps h2 {
+  font-size: 16px;
+}
+
+.steps h2.current {
+  font-size: 20px;
+  color: var(--secondary-main);
+  animation: grow 0.8s linear;
+}
+
+.steps,
+.stepNav {
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+}
+
+.stepNav {
+  height: 48px;
+  gap: 1em;
+}
+
+.stepNav button {
+  width: 8em;
+  height: 36px;
+  border: none;
+  border-radius: 4px;
+  background: var(--primary-main);
+  color: #fefefe;
+  box-shadow: 0 4px 0 #212121;
+  cursor: pointer;
+}
+
+.stepNav button:disabled {
+  opacity: 0.8;
+  cursor: default;
+}
+
+.stepNav button:disabled,
+.stepNav button:active {
+  box-shadow: none;
+  transform: translateY(4px);
+}
+
+.currentStep {
+  padding: 1em;
+  min-height: 16em;
+}
+
+@keyframes grow {
+  0% {
+    text-shadow: none;
+  }
+  50% {
+    text-shadow: 0 0 4px var(--secondary-main);
+  }
+  100% {
+    text-shadow: none;
+  }
+}
+</style>
